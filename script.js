af6102661fabf96473f73fe06242648a82d24985
jackumina <jack.umina@comcast.net>
2024-05-13 18:33:08 -0600

    Working, now need to hook up the database

--- script.js
+++ script.js
@@ -1,25 +1,31 @@
 var startTime;
 var timerInterval;
-var elapsedTime = 0;
+var finalTime = 0;
 var bestScore = 0;
 var started = false;
 
 document.getElementById('start-button').addEventListener('click', function() {
-    // start timer
-    startTimer();
+    if(!started){
+        // start timer
+        startTimer();
+        started = true;
 
-    started = true;
-
-    document.getElementById('start-button').blur();
-
-    // Apply green background color to body
-    document.body.style.backgroundColor = "#00A36C";
+        // Disable start button + blur focus so space bar won't react to last focus click
+        document.getElementById('start-button').disabled = true;
+        document.getElementById('start-button').blur();
     
-    // Make text white
-    document.getElementById('title-text').style.color = "white";
-    document.getElementById('instructions-text').style.color = "white";
-    document.getElementById('high-score-text').style.color = "white";
-    document.getElementById('timer-text').style.color = "white";
+        // Apply green background color to body
+        document.body.style.backgroundColor = "#00A36C";
+        
+        // Make text white
+        document.getElementById('title-text').style.color = "white";
+        document.getElementById('instructions-text').style.color = "white";
+        document.getElementById('high-score-text').style.color = "white";
+        document.getElementById('timer-text').style.color = "white";
+    }
+    else{
+        alert("hit");
+    }
 });
 
 document.addEventListener('keydown', function(event) {
@@ -27,8 +33,10 @@ document.addEventListener('keydown', function(event) {
         
         // stop timer
         stopTimer();
-
         started = false;
+
+        // enable start button
+        document.getElementById('start-button').disabled = false;
 
         // Apply white background color to the body
         document.body.style.backgroundColor = "white";
@@ -43,8 +51,8 @@ document.addEventListener('keydown', function(event) {
 
 function startTimer() {
     startTime = Date.now();
-  
-    // Update timer every millisecond
+
+    // call updateTimer every millisecond
     timerInterval = setInterval(updateTimer, 1);
 }
   
@@ -52,15 +60,17 @@ function stopTimer() {
     // Stop the timer
     clearInterval(timerInterval);
 
-    // Calculate elapsed time
-    var currentTime = Date.now();
-    elapsedTime = currentTime - startTime;
+    // Calculate final time
+    finalTime = Date.now() - startTime;
 
     // Display the formatted time
     updateTimer();
 }
 
 function updateTimer() {
+    // Calculate elapsed time
+    var elapsedTime = Date.now() - startTime;
+
     // Convert milliseconds to minutes, seconds, and milliseconds
     var minutes = Math.floor(elapsedTime / (1000 * 60));
     var seconds = Math.floor((elapsedTime % (1000 * 60)) / 1000);
@@ -72,13 +82,11 @@ function updateTimer() {
     milliseconds = milliseconds < 100 ? (milliseconds < 10 ? '00' + milliseconds : '0' + milliseconds) : milliseconds;
 
     // Display the formatted time
-    document.getElementById('timer').textContent = minutes + ':' + seconds + ':' + milliseconds;
+    document.getElementById('timer').innerHTML = minutes + ':' + seconds + ':' + milliseconds;
 
-    if(elapsedTime < bestScore || bestScore == 0){
+    if(finalTime < bestScore || bestScore == 0){
         bestScore = elapsedTime;
 
-        let multilineString = 'Best Score: ' + '\n' + minutes + ':' + seconds + ':' + milliseconds;
-        multilineString = multilineString.replace(/\n/g, "<br>");
-        document.getElementById("high-score").innerHTML = multilineString;
+        document.getElementById("high-score").innerHTML = 'Best Score: ' + "<br>" + minutes + ':' + seconds + ':' + milliseconds;
     }
 }
